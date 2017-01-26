fe5eef45721dcbb53f3cea813c1b85e34c15b0f0
Gene Der Su <e870252314@yahoo.com.tw>
2017-01-26 11:01:22 -0800

    feat: completed the traffic data Kafka producer
    The script will generate number of cars based on the given parameter.
    It will continuous produce the message to Kafka with random speed and
    the grid id of the car.

---
+++ traffic_simulator/spawn_kafka_streams.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# run by bash spawn_kafka_streams.sh <KAFKA_BROKER_PUBLIC_IP> <NUM_SPAWNS> <SESSION_NAME> <NUM_CARS>
+IP_ADDR=$1
+NUM_SPAWNS=$2
+SESSION=$3
+NUM_CARS=$4
+tmux new-session -s $SESSION -n bash -d
+for ID in `seq 1 $NUM_SPAWNS`;
+do
+    echo $ID
+    tmux new-window -t $ID
+    tmux send-keys -t $SESSION:$ID 'python traffic_simulator.py '"$IP_ADDR"' '"$NUM_CARS"'' C-m
+done
--- traffic_simulator/traffic_simulator.py
+++ traffic_simulator/traffic_simulator.py
@@ -3,76 +3,90 @@ This script is used to mock the traffic data in scaled down version of
 San Francisco. The road map is taken from Google Maps. Each car will
 randomly occupy a space on the road and each road space has a capacity
 of cars that can be traveling. Each car will be traveling for a certain
-distance. Once the full distance is traveled, it will be removed. 
+distance. Once the full distance is traveled, it will be removed and restart
+with a new location. 
 """
 
 import random
 from sf_map import SFMap
 from car import Car
-import matplotlib.pyplot as plt
-import time
 
-t0 = time.time()
+import random
+import sys
 
-# load SFMap test
-my_map = SFMap()
-print len(my_map.map_pixels)
-print my_map.map_pixels[0][0].road_type
-print my_map.map_pixels[90][252].road_type
+from kafka.client import SimpleClient
+from kafka.producer import KeyedProducer
 
-# load cars test
-my_cars = []
-for i in xrange(1000):
-    my_cars.append(Car(i,(i*2, i*3)))
+class Producer(object):
 
-print my_cars[123].distance_to_end, my_cars[123].current_location
+    def __init__(self, addr):
+        self.client = SimpleClient(addr)
+        self.producer = KeyedProducer(self.client)
 
-# car move test
-my_car = Car(1000, my_map.random_location())
-x = []
-y = []
-for i in xrange(1000):
-    new_location = my_map.move_location(my_car.current_location,
-                                        my_car.pervious_location)
-    x.append(new_location[0])
-    y.append(new_location[1])
-    my_car.move(25, new_location)
+    def produce_msgs(self, number_of_cars):
+        # load SFMap test
+        my_map = SFMap()
 
-##plt.plot(x,y)
-##plt.plot(x[0], y[0], '*')
-##plt.plot(x[len(x)-1], y[len(y)-1], '.')
-##plt.show()
+        # generat cars according to the number_of_cars
+        my_cars = []
+        for i in xrange(number_of_cars):
+            my_cars.append(Car(random.randint(1,100), my_map.random_location()))
 
-def location_convertor(location):
-    x = (122.470891 - 122.391583)/(800 - 338)
-    y = -(37.813187 - 37.690489)/(900)
-    x0 = -122.478101 - 295*x
-    y0 = 37.813187
+        # convert the pixel into lat and long
+        def location_convertor(location):
+            x = (122.470891 - 122.391583)/(800 - 338)
+            y = -(37.813187 - 37.690489)/(900)
+            x0 = -122.478101 - 295*x
+            y0 = 37.813187
 
-    return (y0 + location[1]*y, x0 + location[0]*x)
-    
+            return (y0 + location[1]*y, x0 + location[0]*x)
 
-# generat one million car driving data for 3600 steps test
-my_cars = []
-number_of_cars = 1000000
-number_of_steps = 3600
-for i in xrange(number_of_cars):
-    my_cars.append(Car(random.randint(1,100), my_map.random_location()))
-    
-with open('traffic_data.txt', 'w') as out_file:
-    for i in xrange(number_of_steps):
-        for j in xrange(number_of_cars):
-            lat, lon = location_convertor(my_cars[j].current_location)
-            out_file.write("%s, %s, %s, %s, %s\n"%
-                           (j, lat, lon, i, my_cars[j].speed))
-            
-            new_location = my_map.move_location(my_cars[j].current_location,
-                                                my_cars[j].pervious_location)
-            if new_location[0] > 0 and new_location[0] < 899 and \
-               new_location[1] > 0 and new_location[1] < 899:
-                my_cars[j].move(25, new_location)
-            else:
-                my_cars[j] = Car(random.randint(1,100), my_map.random_location())
+        time_field = 0
+        while True:
+            time_field += 1
+            for car_id in xrange(number_of_cars):
+                #lat, lon = location_convertor(my_cars[j].current_location)
+
+                # convert the indices, 10 x 10 grid for now
+                grid_id = my_cars[j].current_location[0]%10 +\
+                          90*(my_cars[j].current_location[1]%10)
+                speed_field = my_cars[car_id].speed
+                str_fmt = "{};{};{};{}"
+                message_info = str_fmt.format(grid_id,
+                                              car_id,
+                                              time_field,
+                                              speed_field)
+                print message_info
+                self.producer.send_messages('auto_log', grid_id, message_info)
+                
+                new_location = my_map.move_location(my_cars[car_id].current_location,
+                                                    my_cars[car_id].pervious_location)
+                if new_location[0] > 0 and new_location[0] < 899 and \
+                   new_location[1] > 0 and new_location[1] < 899:
+                    new_speed = my_cars[car_id].speed + random.randint(-10, 10)
+                    if new_speed < 1:
+                        new_speed = 1
+                    my_cars[car_id].move(25, new_location)
+                else:
+                    my_cars[car_id] = Car(random.randint(1,100), my_map.random_location())
+
+        price_field = random.randint(800,1400)
+        while True:
+            time_field = datetime.now().strftime("%Y%m%d %H%M%S")
+            price_field += random.randint(-10, 10)/10.0
+            volume_field = random.randint(1, 1000)
+            str_fmt = "{};{};{};{}"
+            message_info = str_fmt.format(source_symbol,
+                                          time_field,
+                                          price_field,
+                                          volume_field)
+            print message_info
+            self.producer.send_messages('auto_log', source_symbol, message_info)
 
 
-print time.time() - t0
+if __name__ == "__main__":
+    args = sys.argv
+    ip_addr = str(args[1])
+    number_of_cars = args[2]
+    prod = Producer(ip_addr)
+    prod.produce_msgs(number_of_cars)
