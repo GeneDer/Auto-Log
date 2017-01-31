84717e94a5bec2dd4794becb21fcb55a0e83223f
Gene Der Su <e870252314@yahoo.com.tw>
2017-01-31 11:24:21 -0800

    feat: added map that shows the grid and the traffic information
    The map will now show the average speed and the traffic volume.

--- flaskapp/flaskapp.py
+++ flaskapp/flaskapp.py
@@ -1,17 +1,11 @@
 from flask import Flask, request, g, render_template, redirect, url_for, jsonify, abort
 import redis
+import numpy
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-@app.route('/')
-def index():
-    
-    
-    return render_template("index.html", text1="hello world")
-
-@app.route('/api')
-def api():
+def query_all_data():
     with open("key.txt", 'r') as o:
         for i in o:
             password = i.strip()
@@ -20,18 +14,57 @@ def api():
                           port=6379,
                           password=password)
     
-    j = {}
-    count = 0
-    
+    data = {}
     for key in r.scan_iter():
-        key = r.get(key).split(';')
-        j[key] = {"average speed": val[0],
-                  "volume": val[1]}
-        count += 1
-    print count
+        val = r.get(key).split(';')
+        data[key] = {"average speed": val[0],
+                     "volume": val[1]}
+    return data
+
+
+# convert the pixel into lat and long
+def location_convertor(location):
+    x = (122.470891 - 122.391583)/(800 - 338)
+    y = -(37.813187 - 37.690489)/(900)
+    x0 = -122.478101 - 295*x
+    y0 = 37.813187
+
+    return (y0 + location[1]*y, x0 + location[0]*x)
         
-    
-    return jsonify(data)
+
+@app.route('/')
+def index():
+    import time
+    t0 = time.time()
+    data = query_all_data()
+    print time.time() - t0
+
+    t0 = time.time()
+    grids = []                 
+    for i in xrange(0,901,18):
+        for j in xrange(0,901,18):
+            grid_id = str(50*(i/18) + j/18)
+            lat1, lon1 = location_convertor((i,j))
+            lat2, lon2 = location_convertor((i,j + 18))
+            lat3, lon3 = location_convertor((i + 18,j + 18))
+            lat4, lon4 = location_convertor((i + 18,j))
+            if grid_id in data:
+                speed = "average speed: %s"%data[grid_id]["average speed"]
+                volume = "traffic volume: %s"%data[grid_id]["volume"]
+                grids.append([lat1, lon1, lat2, lon2,
+                              lat3, lon3, lat4, lon4,
+                              speed, volume])
+            else:
+                grids.append([lat1, lon1, lat2, lon2,
+                              lat3, lon3, lat4, lon4,
+                              "average speed: NA", "traffic volume: 0"])
+    print time.time() - t0
+    return render_template("index.html", grids=grids)
+
+
+@app.route('/api')
+def api():    
+    return jsonify(query_all_data())
 
 if __name__ == '__main__':
     app.run()
