5ae1b04aaa0a72463f62731ec845a65456ba53c3
Gene Der Su <e870252314@yahoo.com.tw>
2017-02-06 11:40:51 -0800

    feat: trying to calculate the edges of car connections
    The messages are grouped by their lat, long, and time. Each car in the
    set show all form connection to each other.

--- spark_streaming/src/main/scala/traffic_data.scala
+++ spark_streaming/src/main/scala/traffic_data.scala
@@ -34,18 +34,31 @@ object TrafficDataStreaming {
         val lines = rdd.map(_._2)
         val ticksDF = lines.map( x => {
 				       val tokens = x.split(";")
-                       Tick(((50*((37.813187 - tokens(0).toDouble)/0.00013633111).toInt + ((tokens(1).toDouble + 122.528741387)/0.00017166233).toInt)/18).toString, tokens(4).toDouble, tokens(4).toString)
+                       Tick(((50*((37.813187 - tokens(0).toDouble)/0.00013633111).toInt + ((tokens(1).toDouble + 122.528741387)/0.00017166233).toInt)/18).toString, tokens(4).toDouble, tokens(2).toString)
 				      }).toDF()
         val ticks_per_source_DF = ticksDF.groupBy("grid_id")
                                 .agg(avg("speed"), approxCountDistinct("car_id"))
 	
-	val r = new RedisClient("52.34.86.155", 6379, secret=Option("PUT YOUR PASSWORD HERE"))
-	ticks_per_source_DF.collect().foreach(t => {
-			r.set(t(0),t(1).toString()+";"+t(2).toString())
-		}
-	)
+        val r = new RedisClient("52.34.86.155", 6379, secret=Option("PUT YOUR PASSWORD HERE"))
+        ticks_per_source_DF.collect().foreach(t => {
+                r.set(t(0),t(1).toString()+";"+t(2).toString())
+            }
+        )
 
         ticks_per_source_DF.show()
+
+        // compute graph starts here
+        // get a set of cars in each combination of time and grid
+        val ticks2DF = lines.map( x => {
+                        val tokens = x.split(";")
+                        Tick2(tokens(0)+tokens(1)+tokens(3), tokens(2).toString)
+                        }).toDF()
+        val ticks2_per_source_DF = ticks2DF.groupBy("lat_long_and_time")
+                                    .agg(collect_set("car_id"))
+                                    .orderBy("car_id")
+        ticks2_per_source_DF.show()
+
+
     }
 
     // Start the computation
@@ -55,6 +68,7 @@ object TrafficDataStreaming {
 }
 
 case class Tick(grid_id: String, speed: Double, car_id: String)
+case class Tick2(lat_long_and_time: String, car_id: String)
 
 /** Lazily instantiated singleton instance of SQLContext */
 object SQLContextSingleton {
